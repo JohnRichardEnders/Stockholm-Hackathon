b01f50fbaa44d67533433b81ce183e2097abf594
Avgustina Shevchuk <avgustina.shevchuk@lemon.markets>
2025-09-13 18:13:00 +0200

    make openAPI work

--- backend/services/openapi_service.py
+++ backend/services/openapi_service.py
@@ -1,11 +1,15 @@
 import openai
 from datetime import datetime
+from dotenv import load_dotenv
 from typing import Tuple, Optional, List
 import os
 
 from pydantic import BaseModel
 
 from backend.models import ClaimStatus, Claim, Evidence, ClaimResponse
+
+
+load_dotenv()
 
 class ClaimGPTResponse(BaseModel):
     claim_text: str
@@ -18,39 +22,27 @@ class OpenAIService:
         if not api_key:
             raise ValueError("OpenAI API key must be provided")
 
-        self.client = openai.OpenAI(api_key=self.api_key)
+        self.client = openai.OpenAI(api_key=api_key)
 
     def get_response(self, prompt: str, max_tokens: int = 1000):
         try:
-            print("avgu")
             model = "gpt-4.1"
             response = self.client.responses.parse(
                 model=model,
                 input=[
                     {"role": "system", "content": "Validate the provided claim with up-to-date online resources."},
-                    {"role": "user", "content": prompt, },
+                    {"role": "user", "content": prompt},
                 ],
                 text_format=ClaimGPTResponse,
             )
-
-
-
-            return response.output_parsed
-
+            print(response.output_parsed)
+            return response
 
         except openai.APIError as e:
             return (f"OpenAI API Error: {str(e)}", datetime.now())
         except Exception as e:
             return (f"Error: {str(e)}", datetime.now())
 
-    def print_response(self, prompt: str, max_tokens: int = 1000) -> Tuple[str, datetime]:
-        result = self.get_response(prompt, max_tokens)
-        response_text, timestamp = result
-
-        print(f"Response: {response_text}")
-        return result
-
-
 
 service = OpenAIService()
-service.print_response(prompt="Trump is the 1st president of the US")
+service.get_response(prompt="Trump is the 1st president of the US")
